62fbf365b057eb6d0c64760b15a621bdc35006b0
Zain Haidar <86505295+zainhaidar16@users.noreply.github.com>
2024-08-31 17:30:46 +0200

    chore: Update file paths, add media file processing functionality, and refactor UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,22 @@
 import os
+import time
 import base64
-import requests
 import streamlit as st
-from PIL import Image
-from io import BytesIO
+from dotenv import load_dotenv
+import google.generativeai as genai
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
 from pypdf import PdfMerger
-from dotenv import load_dotenv  # Import the load_dotenv function
+from google.auth import load_credentials_from_file
 
-# Load environment variables from .env file
-load_dotenv()
+# Path to your credentials file
+CREDENTIALS_PATH = 'cred.json'
 
-# Hugging Face API token
-HF_API_TOKEN = os.getenv('HF_API_TOKEN')  # Make sure to set this environment variable
+# Load credentials from the file
+credentials, project = load_credentials_from_file(CREDENTIALS_PATH)
 
-# Hugging Face API endpoints
-ENDPOINTS = {
-    'text-generation': 'https://api-inference.huggingface.co/models/gpt2',
-    'summarization': 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
-    'image-classification': 'https://api-inference.huggingface.co/models/google/vit-base-patch16-224-in21k',
-    'audio-transcription': 'https://api-inference.huggingface.co/models/facebook/wav2vec2-base-960h'
-}
-
-# Helper function to call Hugging Face Inference API
-def call_hf_api(endpoint, data, headers):
-    response = requests.post(endpoint, headers=headers, files=data)
-    return response.json()
+# Initialize Google Cloud Vertex AI with the credentials
+vertexai.init(project=project, location='us-central1', credentials=credentials)
 
 # Custom CSS for styling
 st.markdown("""
@@ -106,14 +98,16 @@ def get_llminfo():
     st.sidebar.header("LLM Configuration")
     st.sidebar.markdown("Configure the language model settings to customize the AI's responses.")
     
-    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=1.0, step=0.25)
+    model_name = st.sidebar.radio("Choose LLM:",
+                                  ("gemini-1.5-flash", "gemini-1.5-pro"))
+    temp = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=1.0, step=0.25)
     topp = st.sidebar.slider("Top P:", min_value=0.0, max_value=1.0, value=0.94, step=0.01)
     maxtokens = st.sidebar.slider("Maximum Tokens:", min_value=100, max_value=5000, value=2000, step=100)
     
-    return temperature, topp, maxtokens
+    return model_name, temp, topp, maxtokens
 
 # Function to handle PDF files
-def handle_pdf_files(uploaded_files, temperature, top_p, max_tokens):
+def handle_pdf_files(uploaded_files, model_name, temperature, top_p, max_tokens):
     st.subheader("📄 PDF File Processing")
     st.write("You can upload multiple PDF files. The files will be merged and processed together.")
     
@@ -135,106 +129,156 @@ def handle_pdf_files(uploaded_files, temperature, top_p, max_tokens):
     merger.write(merged_file)
     merger.close()
 
-    st.write(f"Merged PDF file created at: {merged_file}")
-    
-    # Optionally display the file or provide a download link
     with open(merged_file, "rb") as pdf_file:
-        st.download_button(
-            label="Download Merged PDF",
-            data=pdf_file,
-            file_name="merged_all_pages.pdf",
-            mime="application/pdf"
-        )
-    
-    question = st.text_input("Enter your question and hit return.", help="Ask any question about the content of the PDF file.")
-    if question:
-        headers = {"Authorization": f"Bearer {HF_API_TOKEN}"}
-        response = requests.post(
-            ENDPOINTS['text-generation'],
-            headers=headers,
-            json={"inputs": question}
-        )
-        if response.status_code == 200:
-            result = response.json()
-            st.markdown(result[0]['generated_text'])
-        else:
-            st.error("Error with API request. Please check the API and your request.")
+        encoded_string = base64.b64encode(pdf_file.read()).decode()
 
+    file_1 = Part.from_data(
+        mime_type="application/pdf",
+        data=base64.b64decode(encoded_string)
+    )
+
+    generation_config = {
+        "temperature": temperature,
+        "top_p": top_p,
+        "max_output_tokens": max_tokens,
+    }
+
+    try:
+        gen_model = GenerativeModel(model_name=model_name, generation_config=generation_config)
+        st.write(f"Total tokens submitted: {gen_model.count_tokens(file_1)}")
+        question = st.text_input("Enter your question and hit return.", help="Ask any question about the content of the PDF file.")
+        if question:
+            response = gen_model.generate_content([question, file_1])
+            st.markdown(response.text)
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
 
 # Function to handle image files
-def handle_image_files(image_file, temperature, top_p, max_tokens):
+def handle_image_files(image_file, model_name, temperature, top_p, max_tokens):
     st.subheader("🖼️ Image File Processing")
     if image_file:
-        image = Image.open(image_file)
+        path_to_files = './media/'
+        if not os.path.exists(path_to_files):
+            os.makedirs(path_to_files)
         
-        # Check if image has an alpha channel and convert to RGB if necessary
-        if image.mode in ("RGBA", "P"):
-            image = image.convert("RGB")
+        file_path = os.path.join(path_to_files, image_file.name)
+        with open(file_path, "wb") as f:
+            f.write(image_file.read())
         
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        
-        # Save image to a buffer in JPEG format
-        buffer = BytesIO()
-        image.save(buffer, format="JPEG")
-        buffer.seek(0)
-        
-        headers = {"Authorization": f"Bearer {HF_API_TOKEN}"}
-        response = call_hf_api(
-            ENDPOINTS['image-classification'],
-            data={"file": buffer},
-            headers=headers
-        )
-        
-        st.write("Image Classification Results:")
-        for label in response:
-            st.write(f"Label: {label['label']}, Confidence: {label['score']:.2f}")
-
+        try:
+            image_file = genai.upload_file(path=file_path)
+            while image_file.state.name == "PROCESSING":
+                time.sleep(10)
+                image_file = genai.get_file(image_file.name)
+            if image_file.state.name == "FAILED":
+                raise ValueError(image_file.state.name)
+            
+            prompt2 = st.text_input("Enter your prompt.", help="Describe what you want the AI to do with the image.")
+            if prompt2:
+                generation_config = {
+                    "temperature": temperature,
+                    "top_p": top_p,
+                    "max_output_tokens": max_tokens,
+                }
+                model = genai.GenerativeModel(model_name=model_name, generation_config=generation_config)
+                response = model.generate_content([image_file, prompt2], request_options={"timeout": 600})
+                st.markdown(response.text)
+                genai.delete_file(image_file.name)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 # Function to handle video files
-def handle_video_files(video_file):
+def handle_video_files(video_file, model_name):
     st.subheader("🎥 Video File Processing")
-    st.write("Video file processing is not implemented in this lightweight example.")
+    if video_file:
+        path_to_files = './media/'
+        if not os.path.exists(path_to_files):
+            os.makedirs(path_to_files)
+        
+        file_path = os.path.join(path_to_files, video_file.name)
+        with open(file_path, "wb") as f:
+            f.write(video_file.read())
+        
+        try:
+            video_file = genai.upload_file(path=file_path)
+            while video_file.state.name == "PROCESSING":
+                time.sleep(10)
+                video_file = genai.get_file(video_file.name)
+            if video_file.state.name == "FAILED":
+                raise ValueError(video_file.state.name)
+            
+            prompt3 = st.text_input("Enter your prompt.", help="Describe what you want the AI to do with the video.")
+            if prompt3:
+                model = genai.GenerativeModel(model_name=model_name)
+                st.write("Making LLM inference request...")
+                response = model.generate_content([video_file, prompt3], request_options={"timeout": 600})
+                st.markdown(response.text)
+                genai.delete_file(video_file.name)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 # Function to handle audio files
-def handle_audio_files(audio_file):
+def handle_audio_files(audio_file, model_name):
     st.subheader("🎵 Audio File Processing")
     if audio_file:
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='audio/wav')
+        path_to_files = './media/'
+        if not os.path.exists(path_to_files):
+            os.makedirs(path_to_files)
         
-        headers = {"Authorization": f"Bearer {HF_API_TOKEN}"}
-        response = requests.post(
-            ENDPOINTS['audio-transcription'],
-            headers=headers,
-            files={"file": audio_bytes}
-        )
-        result = response.json()
-        st.write("Audio Transcription:")
-        st.write(result['text'])
+        file_path = os.path.join(path_to_files, audio_file.name)
+        with open(file_path, "wb") as f:
+            f.write(audio_file.read())
+        
+        try:
+            audio_file = genai.upload_file(path=file_path)
+            while audio_file.state.name == "PROCESSING":
+                time.sleep(10)
+                audio_file = genai.get_file(audio_file.name)
+            if audio_file.state.name == "FAILED":
+                raise ValueError(audio_file.state.name)
+            
+            prompt3 = st.text_input("Enter your prompt.", help="Describe what you want the AI to do with the audio.")
+            if prompt3:
+                model = genai.GenerativeModel(model_name=model_name)
+                response = model.generate_content([audio_file, prompt3], request_options={"timeout": 600})
+                st.markdown(response.text)
+                genai.delete_file(audio_file.name)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 # Main function to run the app
 def main():
     page_setup()
     media_type = get_typeofmedia()
-    temperature, top_p, max_tokens = get_llminfo()
+    model_name, temperature, top_p, max_tokens = get_llminfo()
     
     if media_type == "PDF files":
         uploaded_files = st.file_uploader("Choose 1 or more files", accept_multiple_files=True)
         if uploaded_files:
-            handle_pdf_files(uploaded_files, temperature, top_p, max_tokens)
+            handle_pdf_files(uploaded_files, model_name, temperature, top_p, max_tokens)
     
     elif media_type == "Images":
-        image_file = st.file_uploader("Upload your image file.", type=["jpg", "png", "jpeg"])
+        image_file = st.file_uploader("Upload your image file.")
         if image_file:
-            handle_image_files(image_file, temperature, top_p, max_tokens)
+            handle_image_files(image_file, model_name, temperature, top_p, max_tokens)
     
     elif media_type == "Video, mp4 file":
-        st.write("Video file processing is not implemented in this lightweight example.")
+        video_file = st.file_uploader("Upload your video")
+        if video_file:
+            handle_video_files(video_file, model_name)
     
     elif media_type == "Audio files":
-        audio_file = st.file_uploader("Upload your audio", type=["wav", "mp3"])
+        audio_file = st.file_uploader("Upload your audio")
         if audio_file:
-            handle_audio_files(audio_file)
+            handle_audio_files(audio_file, model_name)
 
 if __name__ == '__main__':
+    load_dotenv()  # Load environment variables from .env file
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    
+    if GOOGLE_API_KEY:
+        genai.configure(api_key=GOOGLE_API_KEY)
+    else:
+        st.error("GOOGLE_API_KEY is not set. Please check your .env file.")
+    
     main()
